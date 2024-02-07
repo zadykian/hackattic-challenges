dcecd6b8fd65e05d8181591798b9715e81001c16
zadykian <zadykian@outlook.com>
2024-02-08 01:06:22 +0400

    HelpMeUnpack

--- Hackattic.Challenges.Tests/HelpMeUnpack.cs
+++ Hackattic.Challenges.Tests/HelpMeUnpack.cs
@@ -23,8 +23,31 @@ public sealed class HelpMeUnpack : IDisposable
 
         var responseContent = await problemSetResponse.Content.ReadAsStreamAsync();
         var problemSet = await JsonSerializer.DeserializeAsync<ProblemSet>(responseContent);
+        problemSet.Bytes.Should().NotBeNullOrWhiteSpace();
 
-        problemSet.Bytes.Should().NotBeEmpty();
+        var solution = await Solve(problemSet);
+
+        var submission = await HttpClient.PostAsync(
+            $"help_me_unpack/solve?access_token={accessToken}",
+            new StringContent(JsonSerializer.Serialize(solution))
+        );
+
+        submission.EnsureSuccessStatusCode();
+        var submissionResponseContent = await submission.Content.ReadAsStreamAsync();
+        var submissionResponse = await JsonSerializer.DeserializeAsync<JsonElement>(submissionResponseContent);
+
+        var rejectionReason = submissionResponse.TryGetProperty("rejected", out var rejected)
+            ? rejected.GetString()
+            : string.Empty;
+
+        rejectionReason.Should().BeNullOrWhiteSpace();
+    }
+
+    private static async Task<Solution> Solve(ProblemSet problemSet)
+    {
+        await Task.CompletedTask;
+        var decodedBytes = Convert.FromBase64String(problemSet.Bytes);
+        return default;
     }
 
     void IDisposable.Dispose() => HttpClient.Dispose();
@@ -34,6 +57,28 @@ public sealed class HelpMeUnpack : IDisposable
         [JsonConstructor]
         public ProblemSet(string bytes) => Bytes = bytes;
 
+        [JsonPropertyName("bytes")]
         public string Bytes { get; }
+    }
+
+    private readonly struct Solution
+    {
+        [JsonPropertyName("int")]
+        public int Int { get; init; }
+
+        [JsonPropertyName("uint")]
+        public uint UnsignedInt { get; init; }
+
+        [JsonPropertyName("short")]
+        public short Short { get; init; }
+
+        [JsonPropertyName("float")]
+        public float Float { get; init; }
+
+        [JsonPropertyName("double")]
+        public double Double { get; init; }
+
+        [JsonPropertyName("big_endian_double")]
+        public double BigEndianDouble { get; init; }
     }
 }
