036e043adb3236e5abab1474561c4453f26b36f3
zadykian <zadykian@outlook.com>
2024-02-08 00:22:58 +0400

    HelpMeUnpack

--- Hackattic.Challenges.Tests/HelpMeUnpack.cs
+++ Hackattic.Challenges.Tests/HelpMeUnpack.cs
@@ -1,14 +1,39 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using FluentAssertions;
+using Hackattic.Challenges.Configuration;
 using NUnit.Framework;
 
 namespace Hackattic.Challenges;
 
-public sealed class HelpMeUnpack
+public sealed class HelpMeUnpack : IDisposable
 {
-    
+    private readonly HttpClient HttpClient = new()
+    {
+        BaseAddress = new("https://hackattic.com/challenges/")
+    };
 
     [Test]
-    public void Test1()
+    [CancelAfter(10_000)]
+    public async Task Run()
     {
-        Assert.Pass();
+        var accessToken = AccessToken.Value();
+        var problemSetResponse = await HttpClient.GetAsync($"help_me_unpack/problem?access_token={accessToken}");
+        problemSetResponse.EnsureSuccessStatusCode();
+
+        var responseContent = await problemSetResponse.Content.ReadAsStreamAsync();
+        var problemSet = await JsonSerializer.DeserializeAsync<ProblemSet>(responseContent);
+
+        problemSet.Bytes.Should().NotBeEmpty();
+    }
+
+    void IDisposable.Dispose() => HttpClient.Dispose();
+
+    private readonly struct ProblemSet
+    {
+        [JsonConstructor]
+        public ProblemSet(string bytes) => Bytes = bytes;
+
+        public string Bytes { get; }
     }
 }
